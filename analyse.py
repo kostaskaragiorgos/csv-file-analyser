922f670cbc5df2ae1ddbe14cfdebde02a2630cdd
kostas karagiorgos <34107442+kostaskaragiorgos@users.noreply.github.com>
2020-09-15 18:17:51 +0300

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -3,5 +3,9 @@ import pandas as pd
 def CreateDataFrame(filename):
     return pd.read_csv(filename)
 
-def FindEmptyCells(dataframe):
+def HaveEmptyCells(dataframe):
     return "YES" if dataframe.isnull().any else "NO"
+
+def HaveDuplicates(dataframe):
+    retrun "YES" if dataframe.duplicated().any else "NO"
+
